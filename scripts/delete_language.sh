5e873432560018783a389bdf379a8e7af2d960ea
Petter Rasmussen <petter@hask.no>
2020-11-07 15:08:40 +0100

    Add script for deleting language

--- scripts/delete_language.sh
+++ scripts/delete_language.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-userId="$1"
+languageId="$1"
 
 curl --request DELETE \
      --header 'Authorization: Token tamed-busman-want-vendetta' \
      --header 'Content-type: application/json' \
-     --url "http://localhost:8089/admin/users/${userId}"
+     --url "http://localhost:8089/admin/languages/${languageId}"
